f70058946f211de246065c62aaa994311018fe8f
Tom Ross <tom@umpox.com>
2016-10-25 22:18:24 +0100

    Logic to select nearby blocks, parseInt to avoid string concat

--- js/controls.js
+++ js/controls.js
@@ -1,12 +1,24 @@
 $(document).ready(function(){
     var box = document.getElementsByTagName('a-box');
     var y_cord, x_cord = 1;
+    var aboveBox, belowBox, leftBox, rightBox;
 
     var changeColor = function() {
         this.setAttribute('color', 'green');
         y_cord = this.getAttribute('y');
-        x_cord = this.getAttribute('x');
-        console.log(document.querySelectorAll('[y="' + y_cord + '"][x="' + x_cord + '"]'));
+        x_cord = this.getAttribute('x');       
+
+        var aboveBox = document.querySelectorAll('[y="' + ( parseInt(y_cord)+1 ) + '"][x="' + x_cord + '"]');
+        aboveBox[0].setAttribute('color', 'red');
+
+        var belowBox = document.querySelectorAll('[y="' + ( parseInt(y_cord) - 1 ) + '"][x="' + x_cord + '"]');
+        belowBox[0].setAttribute('color', 'red');
+
+        var leftBox = document.querySelectorAll('[y="' + y_cord + '"][x="' + ( parseInt(x_cord) - 1 ) + '"]');
+        leftBox[0].setAttribute('color', 'red');
+
+        var rightBox = document.querySelectorAll('[y="' + y_cord + '"][x="' + ( parseInt(x_cord) + 1 ) + '"]');
+        rightBox[0].setAttribute('color', 'red');   
     };
     var returnColor = function() {
         this.setAttribute('color', '#4CC3D9');  
