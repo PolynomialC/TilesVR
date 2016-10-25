78246083211877592a7a4282e60fa21ba5366358
Tom Ross <tom@umpox.com>
2016-10-25 22:05:49 +0100

    Logic to select current box by coordinates

--- js/controls.js
+++ js/controls.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
     var box = document.getElementsByTagName('a-box');
+    var y_cord, x_cord = 1;
 
     var changeColor = function() {
         this.setAttribute('color', 'green');
+        y_cord = this.getAttribute('y');
+        x_cord = this.getAttribute('x');
+        console.log(document.querySelectorAll('[y="' + y_cord + '"][x="' + x_cord + '"]'));
     };
     var returnColor = function() {
         this.setAttribute('color', '#4CC3D9');  
