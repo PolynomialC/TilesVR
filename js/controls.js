59b3abf11185733813849ace5d64a0b9a9ca8eea
Tom Ross <tom@umpox.com>
2016-11-15 22:10:25 +0000

    Improve code efficiency

--- js/controls.js
+++ js/controls.js
@@ -1,18 +1,15 @@
 $(document).ready(function(){
     var box = document.getElementsByTagName('a-box');
     var y_cord, x_cord = 1;
-    var max_x = '5';
-    var max_y = '5';
-    var min_x = '1';
-    var min_y = '1';
+    var max_x = '5', max_y = '5';
+    var min_x = '1', min_y = '1';
     var aboveBox, belowBox, leftBox, rightBox;
     var cursor = document.getElementById('cursor');
 
-
-    var changeColour = function() {
-        //DEMO CODE REPLACE SOON
+    var moveBlock = function(currentBlock) {
         this.setAttribute('color', '#88898c');
-        var oldBox = document.querySelectorAll('[y="' + ( parseInt(y_cord)) + '"][x="' + x_cord + '"]');
+       /* var oldBox = document.querySelectorAll('[y="' + ( parseInt(y_cord)) + '"][x="' + x_cord + '"]');
+
         //DEMO CODE REPLACE SOON
         if (this.getAttribute('active') === "true") {
             oldBox[0].setAttribute('color', '#1c1c1f');
@@ -27,56 +24,45 @@ $(document).ready(function(){
         belowBox[0].setAttribute('scale', '1');
         leftBox[0].innerHTML = '';
         leftBox[0].setAttribute('scale', '1');
-        //highlightBlocks(this);
+        //highlightBlocks(this);*/
     };
 
-   /* var returnColor = function() {
-        //this.setAttribute('color', '#4CC3D9');  
+    var calculateNearbyBlocks = function(currentBlock) {
+        var nearbyBlocks = {};
+        y_cord = currentBlock.getAttribute('y');
+        x_cord = currentBlock.getAttribute('x');  
 
-        //RESET SURROUNDING BOX VALUES
-        aboveBox[0].setAttribute('depth', '0.05');
-        belowBox[0].setAttribute('depth', '0.05');
-        leftBox[0].setAttribute('depth', '0.05');
-        rightBox[0].setAttribute('depth', '0.05');
-
-        aboveBox, belowBox, leftBox, rightBox = null;
-    };*/
-
-    var highlightBlocks =  function(emptyBox) {
-        y_cord = emptyBox.getAttribute('y');
-        x_cord = emptyBox.getAttribute('x');    
-
-        //Determine if there is a box alongside current box, if so change its colour
         if (y_cord !== max_y) {
-            aboveBox = document.querySelectorAll('[y="' + ( parseInt(y_cord)+1 ) + '"][x="' + x_cord + '"]');
-            aboveBox[0].classList.add('clickable');
-            aboveBox[0].innerHTML = '<a-animation dur="500" attribute="scale" direction="alternate-reverse" repeat="indefinite" to="1.15 1.15 1.15"></a-animation>';
+            nearbyBlocks.aboveBox = document.querySelectorAll('[y="' + ( parseInt(y_cord)+1 ) + '"][x="' + x_cord + '"]');
         }
         if (y_cord !== min_y) {
-            belowBox = document.querySelectorAll('[y="' + ( parseInt(y_cord) - 1 ) + '"][x="' + x_cord + '"]');
-            belowBox[0].classList.add('clickable');
-            belowBox[0].innerHTML = '<a-animation dur="500" attribute="scale" direction="alternate-reverse" repeat="indefinite" to="1.15 1.15 1.15"></a-animation>';
-        }
+            nearbyBlocks.belowBox = document.querySelectorAll('[y="' + ( parseInt(y_cord) - 1 ) + '"][x="' + x_cord + '"]');
+        }   
         if (x_cord !== min_x) {
-            leftBox = document.querySelectorAll('[y="' + y_cord + '"][x="' + ( parseInt(x_cord) - 1 ) + '"]');
-            leftBox[0].classList.add('clickable');
-            leftBox[0].innerHTML = '<a-animation dur="500" attribute="scale" direction="alternate-reverse" repeat="indefinite" to="1.15 1.15 1.15"></a-animation>';
+            nearbyBlocks.leftBox = document.querySelectorAll('[y="' + y_cord + '"][x="' + ( parseInt(x_cord) - 1 ) + '"]');
         }
         if (x_cord !== max_x) {
-            rightBox = document.querySelectorAll('[y="' + y_cord + '"][x="' + ( parseInt(x_cord) + 1 ) + '"]');
-            rightBox[0].classList.add('clickable');
-            rightBox[0].innerHTML = '<a-animation dur="500" attribute="scale" direction="alternate-reverse" repeat="indefinite" to="1.15 1.15 1.15"></a-animation>';
+            nearbyBlocks.rightBox = document.querySelectorAll('[y="' + y_cord + '"][x="' + ( parseInt(x_cord) + 1 ) + '"]');
         }
+
+        highlightBlocks(nearbyBlocks);                   
+     };
+
+    var highlightBlocks =  function(nearbyBlocks) {   
+        for (var block in nearbyBlocks){
+            nearbyBlocks[block][0].classList.add('clickable');
+            nearbyBlocks[block][0].innerHTML = '<a-animation dur="500" attribute="scale" direction="alternate-reverse" repeat="indefinite" to="1.15 1.15 1.15"></a-animation>';
+        }  
     };
 
     //SET BLOCK STATES
     for (var i = 0; i < box.length; i++) {
         //Add mouse listeners
-        box[i].addEventListener('click', changeColour, false);        
+        box[i].addEventListener('click', moveBlock, false);        
 
         if ( box[i].getAttribute('empty') === "true" ) {
             box[i].setAttribute('color', '#88898c');
-            highlightBlocks(box[i]);
+            calculateNearbyBlocks(box[i]);
         }
         else if ( box[i].getAttribute('active') === "true" ) {
             box[i].setAttribute('color', '#1c1c1f');
@@ -87,5 +73,4 @@ $(document).ready(function(){
     }
 
     cursor.setAttribute('raycaster', 'objects: .clickable');
-
 });
