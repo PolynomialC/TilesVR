90bbb9bd785a3d94f53bcf4ed50e252d7290f1c1
Tom Ross <tom@umpox.com>
2016-11-17 15:35:59 +0000

    Add navigation buttons to menu

--- js/menuLogic.js
+++ js/menuLogic.js
@@ -2,9 +2,18 @@
 var navigateLogin = function() {
     $("#mainMenu").hide();
     $("#mainMenu-loginMenu").show();
+    $("#mainMenu-navButtons").show();
 };
 
-var navigateRegister = function(){
+var navigateRegister = function() {
     $("#mainMenu").hide();
     $("#mainMenu-registerMenu").show();  
+    $("#mainMenu-navButtons").show();
+};
+
+var navigateMenu = function() {
+    $("#mainMenu").show();
+    $("#mainMenu-registerMenu").hide();
+    $("#mainMenu-loginMenu").hide();  
+    $("#mainMenu-navButtons").hide(); 
 };
