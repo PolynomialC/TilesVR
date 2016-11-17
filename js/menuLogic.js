cb7e866ba11bb8b0bf6c3e8f7e5cc9ba7c1a4a32
Tom Ross <tom@umpox.com>
2016-11-17 15:00:55 +0000

    Make site responsive and start menu system

---
+++ js/menuLogic.js
@@ -0,0 +1,10 @@
+"use strict";
+var navigateLogin = function() {
+    $("#mainMenu").hide();
+    $("#mainMenu-loginMenu").show();
+};
+
+var navigateRegister = function(){
+    $("#mainMenu").hide();
+    $("#mainMenu-registerMenu").show();  
+};
